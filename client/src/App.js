5cf53d0ff01285dd816c7f32d92731aa8024d012
Miranda Musselman <mirandamusselman@Pams-MacBook-Pro.local>
2020-02-26 10:30:49 -0700

    cant get it

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import SavedList from "./Movies/SavedList";
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+
+import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
+
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
+  const [savedList, setSavedList] = useState( [] );
+
   const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
+    setSavedList( [...savedList, movie] );
   };
+
   return (
     <div>
-      <Router>
-        <SavedList list={savedList} />
-        <div>
-          {/* <Link to={"/movies/${id}"}>Test</Link> */}
-          <Route exact path="/" component={MovieList} />
-          <Route path="/movies/:id" component={Movie} />
-        </div>
-      </Router>
+      <SavedList list={savedList} />
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
     </div>
   );
 };
-/* <Link to={"/movies/${props.match.params.id}"}> */
+
 export default App;
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,12 +1,13 @@
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState(null);
- 
+  
+  const id = props.match.params.id;
+
   useEffect(() => {
-    // const id =1;
-    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -19,7 +20,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[props.match.params.id]);
+  },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
